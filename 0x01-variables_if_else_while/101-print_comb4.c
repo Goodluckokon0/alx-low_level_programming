607d6cf61d1afa27561c4ef7ca4531f85f052980
Goodluck okon <goodluckokon0@gmail.com>
2022-09-09 22:12:53 +0000

    Task completed

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,8 +21,10 @@ int main(void)
 				putchar((num2 % 10) + '0');
 				putchar((num3 % 10) + '0');
 				if (num1 != 7 || num2 != 8 || num3 != 9)
+				{
 					putchar(',');
-				putchar(' ');
+					putchar(' ');
+				}
 			}
 		}
 	}
