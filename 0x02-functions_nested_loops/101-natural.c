7cf6e9818a128850f7078b2809c6cacb45bfd3dc
Goodluck okon <goodluckokon0@gmail.com>
2022-09-13 22:11:38 +0000

    Task completed

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,13 +7,15 @@
 
 int main(void)
 {
-	int a, b;
+	int a;
 	int sum = 0;
 
 	for (a = 1; a < 1024; a++)
 	{
 		if ((a % 3) == 0 || (a % 5) == 0);
+		{
 			sum += a;
+		}
 	}
 	printf("%d", sum);
 	printf("\n");
