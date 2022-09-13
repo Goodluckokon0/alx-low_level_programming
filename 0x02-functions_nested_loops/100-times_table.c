3448878e40b61fc55ceb1047c41e980f9ae5c9a8
Goodluck okon <goodluckokon0@gmail.com>
2022-09-13 21:40:59 +0000

    Task completed

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,35 +14,30 @@ void print_times_table(int n)
 	{
 		for (a = 0; a <= n; a++)
 		{
-			for (b = 0; b <= n; b++)
+			_putchar('0');
+			for (b = 1; b <= n; b++)
 			{
 				c = a * b;
 				if (c < 10)
 				{
-					_putchar(c + 48);
-					if (b == n)
-						continue;
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
+					_putchar(c + 48);
 				}
 				else if (c > 9 && c < 100)
 				{
-					_putchar((c / 10) + 48);
-					_putchar((c % 10) + 48);
-					if (b == n)
-						continue;
 					_putchar(',');
 					_putchar(' ');
+					_putchar((c / 10) + 48);
+					_putchar((c % 10) + 48);
 				}
 				else if (c > 99 && c < 1000)
 				{
+					_putchar(',');
 					_putchar((c / 10) / 10 + 48);
 					_putchar((c / 10) % 10 + 48);
 					_putchar((c % 10) + 48);
-					if (b == n)
-						continue;
-					_putchar(',');
 				}
 			}
 			_putchar('\n');
