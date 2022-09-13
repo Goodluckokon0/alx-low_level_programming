51555db537a48d78278aa95c8b75389bbff27018
Goodluck okon <goodluckokon0@gmail.com>
2022-09-13 21:26:43 +0000

    Task completed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,16 +12,24 @@ void print_to_98(int n)
 	if (n >= 98)
 	{
 		for (; n >= 98; n--)
+		{
 			printf("%d", n);
-		if (n != 98)
-		printf(", ");
+			if (n != 98)
+			{
+				printf(", ");
+			}
+		}
 	}
 	else if (n < 98)
 	{
 		for (; n <= 98; n++)
+		{
 			printf("%d", n);
-		if (n != 98)
-		printf(", ");
+			if (n != 98)
+			{
+				printf(", ");
+			}
+		}
 	}
 	printf("\n");
 }
