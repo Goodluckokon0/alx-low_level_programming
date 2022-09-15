3837a9ffd6ce097adff8082a8567d686bbc45ebf
Goodluck okon <goodluckokon0@gmail.com>
2022-09-15 20:32:59 +0000

    Task completed

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,12 +7,17 @@
 
 void print_line(int n)
 {
+	int i;
+
+
 	if (n <= 0)
 		_putchar('\n');
 	else if (n > 0)
 	{
-		for (; n > 0; n--)
+		for (i = 1; i <= n; i++)
+		{
 			_putchar('_');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
