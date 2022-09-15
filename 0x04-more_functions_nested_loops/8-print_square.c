f1e13f526a8d428a2c63a19d345540fcd4b950e4
Goodluck okon <goodluckokon0@gmail.com>
2022-09-15 21:01:32 +0000

    commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,9 +11,8 @@ void print_square(int size)
 
 	if (size <= 0)
 		_putchar('\n');
-	else if (size > 0)
-	{
-		for (0 = 1; a <= size; a++)
+	else
+		for (a = 0; a <= size; a++)
 		{
 			for (b = 0; b <= size; b++)
 			{
