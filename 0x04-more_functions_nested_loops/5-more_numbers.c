92801368854502f56ab0697b7d389c53869f8a77
Goodluck okon <goodluckokon0@gmail.com>
2022-09-15 20:24:04 +0000

    Task completed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,23 +7,19 @@
 
 void more_numbers(void)
 {
-	int num1, num2;
+	int num1;
+	char num2;
 
 	for (num1 = 1; num1 <= 10; num1++)
 	{
 		for (num2 = 0; num2 <= 14; num2++)
 		{
-			if (num2 < 10)
-			{
-				_putchar((num2 % 10) + 48);
-			}
-			else if (num2 > 9)
+			if (num2 / 10 > 0)
 			{
 				_putchar((num2 / 10) + 48);
-				_putchar((num2 % 10) + 48);
 			}
+			_putchar((num2 % 10) + 48);
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
