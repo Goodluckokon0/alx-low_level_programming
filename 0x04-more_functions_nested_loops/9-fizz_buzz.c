6ec74df267a6dab99daa0bacc329a663827f18ea
Goodluck okon <goodluckokon0@gmail.com>
2022-09-15 21:17:04 +0000

    task completed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,14 +15,15 @@ int main(void)
 	for (a = 0; a <= 100; a++)
 	{
 		if ((a % 3) == 0 && (a % 5) == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (a % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (a % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else
-			printf("%d ", a);
+			printf("%d", a);
+		if (a != 100)
+			printf(" ");
 	}
-	printf("\n");
 	return (0);
 }
