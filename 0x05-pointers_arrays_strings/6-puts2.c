3afddd22c7bed7bb2077b9ea7ab0caa7549a7207
Goodluck okon <goodluckokon0@gmail.com>
2022-09-20 00:13:16 +0000

    task completed

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,7 +10,7 @@ void puts2(char *str)
 {
 	int count;
 
-	for (count = 0; str[count] != '\0'; count += 2)
+	for (count = 0; str[count] <= '\0'; count += 2)
 		_putchar(str[count]);
 	_putchar('\n');
 }
