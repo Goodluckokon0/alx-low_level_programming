2cf3edfcf3c2f9aac3b6cf511d45072a336a72b1
Goodluck okon <goodluckokon0@gmail.com>
2022-09-22 21:41:59 +0000

    Task completed

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,13 +11,12 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int index;
-	int len = 0;
 
 	for (index = 0; src[index] != '\0' && index < n; index++)
 	{
 		dest[index] = src[index];
 	}
-	for (index = 0; index < n; index++)
+	for (; index < n; index++)
 		dest[i] = '\0';
 
 	return (dest);
