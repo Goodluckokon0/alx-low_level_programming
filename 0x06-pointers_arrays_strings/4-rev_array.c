8c64fe22e20b3009756902a7492dde62ef896957
Goodluck okon <goodluckokon0@gmail.com>
2022-09-22 21:51:36 +0000

    Task completed

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,10 +10,10 @@ void reverse_array(int *a, int n)
 	int i;
 	int tmp;
 
-	for (i = 0; a[i] < n / 2; i++)
+	for (i = 0; i < n / 2; i++)
 	{
 		tmp = a[i];
-		a[i] = a[n - 1];
-		a[n - 1 - 1] = tmp;
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = tmp;
 	}
 }
