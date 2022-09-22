d04a47a443a58eb0f2829792f517b9458146a956
Goodluck okon <goodluckokon0@gmail.com>
2022-09-22 21:07:59 +0000

    Task completed

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,11 +9,15 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int index;
+	int index = 0;
+	int len;
+
+	while (dest[len] != '\0')
+		len++;
 
 	for (index = 0; src[index] < n; index++)
 	{
-		dest[index] = src[index];
+		dest[len + index] = src[index];
 	}
 	return (dest);
 }
