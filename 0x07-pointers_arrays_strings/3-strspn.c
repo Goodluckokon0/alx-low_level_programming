3bc40ffbb75cf2272c1a921e25ffaa9510571616
Goodluck okon <goodluckokon0@gmail.com>
2022-09-26 23:06:24 +0000

    Task done

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,21 +9,15 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j, bool;
+	unsigned int i, j, byte = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		bool = 1;
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
-			{
-				bool = 0;
-				break;
-			}
+				byte++;
 		}
-		if (bool = 1)
-			break;
 	}
-	return (i);
+	return (byte);
 }
