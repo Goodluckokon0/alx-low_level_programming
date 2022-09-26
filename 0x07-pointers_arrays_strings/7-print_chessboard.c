b014e47bc28d148f4b5b9f647131334e93a693d8
Goodluck okon <goodluckokon0@gmail.com>
2022-09-26 22:28:04 +0000

    Task done

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,13 +6,13 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
+	unsigned int i, j;
 
 	for (i = 1; i <= 8; i++)
 	{
 		for (j = 1; j <= 8; j++)
 		{
-			_putchar(a[i % 10][j % 10]);
+			_putchar(a[i][j]);
 		}
 		_putchar('\n');
 	}
