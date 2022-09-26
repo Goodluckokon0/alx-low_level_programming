164e5f1b550dde1cf1fdded5ec01973643b5b774
Goodluck okon <goodluckokon0@gmail.com>
2022-09-26 23:10:04 +0000

    Task done

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,12 +11,16 @@ void print_diagsums(int *a, int size)
 {
 	int i, sum1 = 0, sum2 = 0;
 
-	for (i = 0; i < (size * size); i++)
+	for (i = 0; i < size; i++)
 	{
-		if (i % (size + 1) == 0)
-			sum1 += a[i];
-		if (i % (size - 1) == 0 && i < size * size - 1)
-			sum2 += a[i];
+		sum1 += a[i];
+		a += size;
+	}
+	a -= size;
+	for (i = 0; i < size; i++)
+	{
+		sum2 += a[i];
+		a -= size;
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
