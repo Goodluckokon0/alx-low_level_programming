1de67a9bac168b97fc520f74047e9a8dc9bcbc02
Goodluck okon <goodluckokon0@gmail.com>
2022-09-27 12:56:03 +0000

    Task completed

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,7 +7,8 @@
  */
 char *rot13(char *str)
 {
-	int i = 0, j, k;
+	int i = 0;
+	int j;
 	char alpha[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
 		'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
 		'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
