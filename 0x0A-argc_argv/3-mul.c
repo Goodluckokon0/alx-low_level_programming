e4f48ee7ebb24d0a5c722dff0d0a2e9b80b3759a
Goodluck okon <goodluckokon0@gmail.com>
2022-09-30 22:59:21 +0000

    Task done

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,15 +10,11 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, mul;
-
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-		mul = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n", mul);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
