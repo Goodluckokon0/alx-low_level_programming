5477225e0c2c6b0bf1641a6f9b353fa3b784b9ec
Goodluck okon <goodluckokon0@gmail.com>
2022-10-11 20:09:42 +0000

    done

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - pronts a struct dog
+ * @d: variable of type struct
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	if (d->name == NULL)
+		d->name = "(nil)";
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - functiomn that frees dogs of type struct
+ * @d: the variable of type struct to free
+ */
+void free_dog(dog_t *d)
+{
+	if(d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
