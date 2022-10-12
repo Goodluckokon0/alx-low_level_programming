a0158e0d9b8ddbc3607f766e321d957701a00b41
Goodluck okon <goodluckokon0@gmail.com>
2022-10-12 22:59:29 +0000

    done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array && action)
 	{
-		if (i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 		{
 			action(array[i]);
 		}	
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,11 +9,11 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-		{"+", op_add};
-		{"-", op_sub};
-		{"*", op_mul};
-		{"/", op_div};
-		{"%", op_mod};
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
 		{NULL, NULL}
 	};
 	int i;
@@ -23,7 +23,7 @@ int (*get_op_func(char *s))(int, int)
 	{
 		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
-		i++:
+		i++;
 	}
 	return (NULL);
 }
